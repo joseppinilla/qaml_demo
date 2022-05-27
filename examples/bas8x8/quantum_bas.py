72fb0b0dd18666ef9b4194cc86581bee7887892f
Jose Pinilla <jpinilla@ece.ubc.ca>
2022-05-27 14:50:28 -0700

    Easier parametrization and iteration BAS(8,8)

--- examples/bas8x8/quantum_bas.py
+++ examples/bas8x8/quantum_bas.py
@@ -5,10 +5,9 @@
 # Developed by: Jose Pinilla
 # %%
 # Required packages
+import os
 import qaml
 import torch
-SEED = 0
-torch.manual_seed(SEED) # For deterministic weights
 
 import matplotlib.pyplot as plt
 import torchvision.transforms as torch_transforms
@@ -37,94 +36,126 @@ train_loader = torch.utils.data.DataLoader(train_dataset,sampler=train_sampler,
                                            batch_size=BATCH_SIZE)
 
 # %%
-################################# Model Definition #############################
-# Specify model with dimensions
-rbm = qaml.nn.RBM(DATA_SIZE,HIDDEN_SIZE)
+# Experiment iterators
+scaling = [True,False]
+betas = [1.0,2.0,3.0,4.0]
+seeds = [1,2,3]
+weight_inits = [0.1,0.5,1.0,2.0,4.0]
 
-# Initialize biases
-weight_init = 1.0
-_ = torch.nn.init.uniform_(rbm.b,-weight_init,weight_init)
-_ = torch.nn.init.uniform_(rbm.c,-weight_init,weight_init)
-_ = torch.nn.init.uniform_(rbm.W,-weight_init,weight_init)
-
-# Set up optimizers
-optimizer = torch.optim.SGD(rbm.parameters(), lr=learning_rate*weight_init,
-                            weight_decay=weight_decay,momentum=momentum)
-
-# Set up training mechanisms
-beta=2.5
+# QAP settings
+chain_strength = 1.6
+num_reads = BATCH_SIZE
 solver_name = "Advantage_system4.1"
-qa_sampler = qaml.sampler.QASampler(rbm,solver=solver_name,beta=beta)
 
-# Loss and autograd
-CD = qaml.autograd.SampleBasedConstrastiveDivergence()
+for auto_scale in scaling:
+    for beta in betas:
+        for weight_init in weight_inits:
 
-# # %%
-auto_scale = True
-# ################################## Model Training ##############################
-# Set the model to training mode
-rbm.train()
-err_log = []
-accuracy_log = []
-b_log = [rbm.b.detach().clone().numpy()]
-c_log = [rbm.c.detach().clone().numpy()]
-W_log = [rbm.W.detach().clone().numpy().flatten()]
-for t in range(EPOCHS):
-    epoch_error = 0
-    for img_batch, labels_batch in train_loader:
-        input_data = img_batch.flatten(1)
+            directory = f"BAS88_lrWbeta{beta}_bcW{weight_init}"
+            directory += f"{'' if auto_scale else 'no'}scale_"
+            directory += f"{EPOCHS}_{'batch' if num_reads==BATCH_SIZE else num_reads}"
+            directory += f"Adv_wd{weight_decay}"
+            directory += f"_cs{chain_strength}"
+            directory = directory.replace('.','')
+            print(directory)
 
-        # Negative Phase
-        vk, prob_hk = qa_sampler(num_reads=BATCH_SIZE,auto_scale=auto_scale,
-                                 num_spin_reversal_transforms=4)
-        # Positive Phase
-        scale = qa_sampler.scalar*qa_sampler.beta if auto_scale else 1.0
-        v0, prob_h0 = input_data, rbm(input_data,scale=scale)
+            for SEED in seeds:
+                ######################################## RNG ###################################
+                torch.manual_seed(SEED)
+                ############################## Logging Directory ###############################
+                if not os.path.exists(directory):
+                        os.makedirs(directory)
+                if not os.path.exists(f'{directory}/{SEED}'):
+                        os.makedirs(f'{directory}/{SEED}')
 
-        # Reconstruction error from Contrastive Divergence
-        err = CD.apply((v0,prob_h0), (vk,prob_hk), *rbm.parameters())
+                print(f"Running with SEED={SEED}")
+                ################################# Model Definition #############################
+                # Specify model with dimensions
+                rbm = qaml.nn.RBM(DATA_SIZE,HIDDEN_SIZE)
+                # Set up optimizers
+                optimizer = torch.optim.SGD(rbm.parameters(), lr=learning_rate*weight_init,
+                                            weight_decay=weight_decay,momentum=momentum)
 
-        # Do not accumulate gradients
-        optimizer.zero_grad()
+                # Set up training mechanisms
+                qa_sampler = qaml.sampler.QASampler(rbm,solver=solver_name,beta=beta)
 
-        # Compute gradients
-        err.backward()
+                # Loss and autograd
+                CD = qaml.autograd.SampleBasedConstrastiveDivergence()
 
-        # Update parameters
-        optimizer.step()
+                # Initialize biases
+                _ = torch.nn.init.uniform_(rbm.b,-weight_init,weight_init)
+                _ = torch.nn.init.uniform_(rbm.c,-weight_init,weight_init)
+                _ = torch.nn.init.uniform_(rbm.W,-weight_init,weight_init)
 
-        #Accumulate error for this epoch
-        epoch_error  += err.item()
+                ################################## Model Training ##############################
+                try:
+                    # Set the model to training mode
+                    rbm.train()
+                    err_log = []
+                    scalar_log = []
+                    accuracy_log = []
+                    b_log = [rbm.b.detach().clone().numpy()]
+                    c_log = [rbm.c.detach().clone().numpy()]
+                    W_log = [rbm.W.detach().clone().numpy().flatten()]
+                    for t in range(EPOCHS):
+                        epoch_error = 0
+                        for img_batch, labels_batch in train_loader:
+                            input_data = img_batch.flatten(1)
 
-    # Error Log
-    b_log.append(rbm.b.detach().clone().numpy())
-    c_log.append(rbm.c.detach().clone().numpy())
-    W_log.append(rbm.W.detach().clone().numpy().flatten())
-    err_log.append(epoch_error)
-    print(f"Epoch {t} Reconstruction Error = {epoch_error}")
-    ############################## CLASSIFICATION ##################################
-    count = 0
-    for test_data, test_label in test_dataset:
-        test_data[-2:,-1] = 0.5
-        recon_hk = rbm(test_data.flatten(),scale=scale)
-        label_pred = rbm.generate(recon_hk,scale=scale).view(*SHAPE)[-2:,-1]
-        if label_pred.argmax() == test_label.argmax():
-            count+=1
-    accuracy_log.append(count/TEST)
-    print(f"Testing accuracy: {count}/{TEST} ({count/TEST:.2f})")
+                            # Negative Phase
+                            vk, prob_hk = qa_sampler(num_reads=num_reads,auto_scale=auto_scale,
+                                                     num_spin_reversal_transforms=4,embed_kwargs={'chain_strength':chain_strength})
+                            # Positive Phase
+                            scale = qa_sampler.scalar*qa_sampler.beta if auto_scale else 1.0
+                            v0, prob_h0 = input_data, rbm(input_data,scale=scale)
 
-# %%
-############################ MODEL VISUALIZATION ###############################
-# Testing accuracy grapha
-fig, ax = plt.subplots()
-plt.plot(accuracy_log)
-plt.ylabel("Testing Accuracy")
-plt.xlabel("Epoch")
-plt.savefig("quantum_accuracy.pdf")
+                            # Reconstruction error from Contrastive Divergence
+                            err = CD.apply((v0,prob_h0), (vk,prob_hk), *rbm.parameters())
 
-# L1 error graph
-fig, ax = plt.subplots()
-plt.plot(err_log)
-plt.ylabel("Reconstruction Error (L1)")
-plt.xlabel("Epoch")
-plt.savefig("quantum_err_log.pdf")
+                            # Do not accumulate gradients
+                            optimizer.zero_grad()
+
+                            # Compute gradients
+                            err.backward()
+
+                            # Update parameters
+                            optimizer.step()
+
+                            #Accumulate error for this epoch
+                            epoch_error  += err.item()
+
+                        # Error Log
+                        b_log.append(rbm.b.detach().clone().numpy())
+                        c_log.append(rbm.c.detach().clone().numpy())
+                        W_log.append(rbm.W.detach().clone().numpy().flatten())
+                        err_log.append(epoch_error)
+                        if auto_scale is True:
+                            scalar_log.append(qa_sampler.scalar)
+                        print(f"Epoch {t} Reconstruction Error = {epoch_error}")
+                        ############################## CLASSIFICATION ##################################
+                        count = 0
+                        for test_data, test_label in test_dataset:
+                            test_data[-2:,-1] = 0.5
+                            recon_hk = rbm(test_data.flatten(),scale=scale)
+                            label_pred = rbm.generate(recon_hk,scale=scale).view(*SHAPE)[-2:,-1]
+                            if label_pred.argmax() == test_label.argmax():
+                                count+=1
+                        accuracy_log.append(count/TEST)
+                        print(f"Testing accuracy: {count}/{TEST} ({count/TEST:.2f})")
+
+                except Exception as e:
+                    print(e)
+                ############################ Store Model and Logs ##############################
+                torch.save(b_log,f"./{directory}/{SEED}/b.pt")
+                torch.save(c_log,f"./{directory}/{SEED}/c.pt")
+                torch.save(W_log,f"./{directory}/{SEED}/W.pt")
+                torch.save(err_log,f"./{directory}/{SEED}/err.pt")
+                torch.save(scalar_log,f"./{directory}/{SEED}/scalar.pt")
+                torch.save(accuracy_log,f"./{directory}/{SEED}/accuracy.pt")
+
+                # Testing accuracy graph
+                fig, ax = plt.subplots()
+                plt.plot(accuracy_log)
+                plt.ylabel("Testing Accuracy")
+                plt.xlabel("Epoch")
+                plt.savefig(f"./{directory}/{SEED}/accuracy.pdf")
